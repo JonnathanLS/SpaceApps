11d807320a259b6ab4609873ae56fec899980d3d
Jonnathan Lacerda Santos <jonnathan.lacerda.santos@gmail.com>
2018-10-21 03:00:13 -0300

    Relatorio de Lançamentos por Agencia e Sites de Lançamentos criados

---
+++ Web/Launcher/Launcher/Controllers/SitesLancamentosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Launcher.Controllers
+{
+    public class SitesLancamentosController : Controller
+    {
+        // GET: SitesLancamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Web/Launcher/Launcher/Models/Home/IndexVm.cs
+++ Web/Launcher/Launcher/Models/Home/IndexVm.cs
@@ -12,12 +12,12 @@ namespace Launcher.Models.Home
         public IndexVm()
         {
             ListaGrids = new List<Grid>();
-            ListaGrids.Add(new Grid() { Text = "Lançamentos Futuros", UrlImagem = "https://cdnbr1.img.sputniknews.com/images/980/23/9802340.jpg", UrlPagina = "LancamentosFuturos/Index" });
-            ListaGrids.Add(new Grid() { Text = "Espaçoporto", UrlImagem = "https://vignette.wikia.nocookie.net/pt.starwars/images/9/9d/Pallista_Spaceport.png/revision/latest?cb=20151103160200", UrlPagina = "EspacoPorto/Index" });
+            ListaGrids.Add(new Grid() { Text = "Lançamentos Futuros", UrlImagem = "https://cdnbr1.img.sputniknews.com/images/980/23/9802340.jpg", UrlPagina = "LancamentosFuturos" });
+            ListaGrids.Add(new Grid() { Text = "Espaçoporto", UrlImagem = "https://vignette.wikia.nocookie.net/pt.starwars/images/9/9d/Pallista_Spaceport.png/revision/latest?cb=20151103160200", UrlPagina = "EspacoPorto" });
             ListaGrids.Add(new Grid() { Text = "Foguetes", UrlImagem = "https://static.vecteezy.com/system/resources/previews/000/153/887/non_2x/vector-rocket-launching-cape-canaveral.jpg" });
-            ListaGrids.Add(new Grid() { Text = "Sites de Lançamento", UrlImagem = "https://cache.olhardigital.com.br/uploads/acervo_imagens/2018/04/20180423111710_660_420.jpg" });
+            ListaGrids.Add(new Grid() { Text = "Sites de Lançamento", UrlImagem = "https://cache.olhardigital.com.br/uploads/acervo_imagens/2018/04/20180423111710_660_420.jpg", UrlPagina = "SitesLancamentos" });
             ListaGrids.Add(new Grid() { Text = "Acompanhe um Lançamento", UrlImagem = "https://imagens.canaltech.com.br/119183.200755-Falcon-9.jpg" });
-            ListaGrids.Add(new Grid() { Text = "Entre em Contato", UrlImagem = "http://www.aptechsp.com.br/wp-content/uploads/2015/03/procura.jpg" });
+            ListaGrids.Add(new Grid() { Text = "Lançamentos por Agência", UrlImagem = "https://www.avibras.com.br/site/images/programa_espacial.jpg", UrlPagina = "LancamentosAgencias" });
 
         }
     }
